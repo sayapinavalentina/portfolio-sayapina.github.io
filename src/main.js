c5a396ff7bad647b56e9d35b4e3dd33b521f51f4
sayapinavalentina <sayapinavalentina7@gmail.com>
2023-03-29 04:38:15 +0300

    create folders and some elements

---
+++ src/main.js
@@ -0,0 +1,11 @@
+
+import '../styles/style.css';
+
+
+import mobileNav from './utils/mobile-nav';
+import darkMode from './utils/dark-mode';
+import lazyLoading from './utils/lazy-loading';
+
+mobileNav();
+darkMode();
+lazyLoading();
